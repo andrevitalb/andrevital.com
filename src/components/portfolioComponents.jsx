6b94f18d12b3d021566783f201f4f54dae134386
Andre Vital <santiago_vital@outlook.com>
2021-05-27 02:23:30 -0500

    Reestructured portfolio components to use projects context

--- src/components/portfolioComponents.jsx
+++ src/components/portfolioComponents.jsx
@@ -1,4 +1,8 @@
+import React, { Fragment, useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+
+// Contexts
+import { useDevelopProjects, usePhotographyProjects } from '../contexts/ProjectsContext';
 
 export const PortfolioProject = ({ name, title, tags, imgSrc }) => {
     const openProject = (project) => {
@@ -11,13 +15,14 @@ export const PortfolioProject = ({ name, title, tags, imgSrc }) => {
 
         prevButton.classList.remove('hidden');
         nextButton.classList.remove('hidden');
-        
+
         if(modals[0] === selectedModal) prevButton.classList.add('hidden');
         else if(modals[modals.length - 1] === selectedModal) nextButton.classList.add('hidden');
 
         modalContainer.classList.add('open');
         selectedModal.classList.add('active');
-    }
+    };
+
     return (
         <div className="portfolio__project" onClick={() => openProject(name)}>
             <div className="portfolio__project__image-container">
@@ -26,32 +31,32 @@ export const PortfolioProject = ({ name, title, tags, imgSrc }) => {
             <div className="portfolio__project__content">
                 <h3 className="portfolio__project__title">{ title }</h3>
                 <p className="portfolio__project__tags">
-                    {tags.map((tag) => <span key={ tag }className="portfolio__project__tags__tag">{ tag }</span>)}
+                    {tags.map((tag) => <span key={ tag } className="portfolio__project__tags__tag">{ tag }</span>)}
                 </p>
             </div>
         </div>
     )
 };
 
-export const PortfolioModal = ({ name, title, text, imgSrc, tags, localUrl, externalUrl }) => {
+export const PortfolioModal = ({ name, title, description, url, tags, type, externalUrl }) => {
     return (
         <div className="portfolio__modal" data-object={ name }>
             <div className="portfolio__modal__holder">
                 <div className="portfolio__modal__image-container">
-                    <img src={ imgSrc } alt={ title } className="portfolio__modal__image"/>
+                    <img src={ `/assets/images/portfolio/${url}/${name}/thumbnail.jpg` } alt={ title } className="portfolio__modal__image"/>
                 </div>
                 <div className="portfolio__modal__content">
                     <div className="portfolio__modal__text-container">
                         <div className="portfolio__modal__header">
                             <h3 className="portfolio__modal__title">{ title }</h3>
                             <h4 className="portfolio__modal__header__tags">
-                                { tags.map((tag) => <span key={ tag }className="portfolio__modal__tag">{ tag }</span>) }
+                                { tags.map((tag) => <span key={ tag } className="portfolio__modal__tag">{ tag }</span>) }
                             </h4>
                         </div>
-                        <p className="portfolio__modal__description">{ text }</p>
+                        <p className="portfolio__modal__description">{ description }</p>
                     </div>
                     <div className="portfolio__modal__cta-container">
-                    <Link to={ `${localUrl}/${name}` } className="portfolio__modal__cta">
+                    <Link to={ `${type}/${name}` } className="portfolio__modal__cta">
                         <p className="portfolio__modal__cta__text">Ver proyecto</p>
                     </Link>
                     <a href={ externalUrl } className="portfolio__modal__cta" target="_blank" rel="noreferrer">
@@ -62,4 +67,82 @@ export const PortfolioModal = ({ name, title, text, imgSrc, tags, localUrl, exte
             </div>
         </div>
     )
+};
+
+export const PortfolioList = ({ match }) => {
+    const [url, setUrl] = useState();
+    const developProjects = useDevelopProjects();
+    const photographyProjects = usePhotographyProjects();
+
+    const initPortfolio = useRef(null);
+
+    if(!initPortfolio.current) {
+        initPortfolio.current = () => {
+            match.url === '/desarrollo' ? setUrl('develop') : setUrl('photography');
+        }
+    }
+
+    const hideModal = (source = 'close') => {
+        const modalContainer = document.querySelector('.portfolio__modal-container');
+        if(source === 'close' || source.target === modalContainer) {
+            const currentModal = document.querySelector('.portfolio__modal.active');
+
+            modalContainer.classList.remove('open');
+            currentModal.classList.remove('active');
+        }
+    };
+
+    const changeActiveProject = (direction) => {
+        const currentModal = document.querySelector('.portfolio__modal.active');
+        const prevButton = document.querySelector('.portfolio__modal-container__button--nav--prev');
+        const nextButton = document.querySelector('.portfolio__modal-container__button--nav--next');
+
+        currentModal.classList.remove('active');
+        prevButton.classList.remove('hidden');
+        nextButton.classList.remove('hidden');
+
+        if(direction === 'prev') {
+            const firstModal = document.querySelector('.portfolio__modal:first-child');
+
+            currentModal.previousElementSibling.classList.add('active');
+            if(firstModal === currentModal.previousElementSibling) prevButton.classList.add('hidden');
+        } else if(direction === 'next') {
+            const lastModal = document.querySelector('.portfolio__modal:nth-last-of-type(4)');
+
+            currentModal.nextElementSibling.classList.add('active');
+            if(lastModal === currentModal.nextElementSibling) nextButton.classList.add('hidden');
+        }
+    };
+
+    useEffect(() => {
+        initPortfolio.current();
+    }, []);
+
+    return (
+        <section className="portfolio">
+            <div className="portfolio__container">
+                {
+                    url === 'develop' ? 
+                    developProjects.map(({ name, title, tags }) => <PortfolioProject key={ name } name={ name } title={ title } tags={ tags } imgSrc={ `/assets/images/portfolio/${url}/${name}/thumbnail.jpg` }/>) :
+                    photographyProjects.map(({ name, title, tags }) => <PortfolioProject key={ name } name={ name } title={ title } tags={ tags } imgSrc={ `/assets/images/portfolio/${url}/${name}/thumbnail.jpg` }/>)
+                }
+            </div>
+            <div className="portfolio__modal-container" onClick={(event) => hideModal(event) }>
+                {
+                    url === 'develop' ? 
+                    developProjects.map(({ name, title, description, tags, externalUrl }) => <PortfolioModal key={ name } name={ name } title={ title } description={ description } url={ url } tags={ tags } type={ match.url } externalUrl={ externalUrl }/>) :
+                    photographyProjects.map(({ name, title, description, tags, externalUrl }) => <PortfolioModal key={ name } name={ name } title={ title } description={ description } url={ url } tags={ tags } type={ match.url } externalUrl={ externalUrl }/>)
+                }
+                <div className="portfolio__modal-container__button portfolio__modal-container__button--close" title="Cerrar" onClick={() => hideModal('close') }>
+                    <i className="fal fa-times"></i>
+                </div>
+                <div className="portfolio__modal-container__button portfolio__modal-container__button--nav portfolio__modal-container__button--nav--prev" title="Anterior" onClick={() => changeActiveProject('prev') }>
+                    <i className="fal fa-chevron-left"></i>
+                </div>
+                <div className="portfolio__modal-container__button portfolio__modal-container__button--nav portfolio__modal-container__button--nav--next" title="Siguiente" onClick={() => changeActiveProject('next') }>
+                    <i className="fal fa-chevron-right"></i>
+                </div>
+            </div>
+        </section>
+    );
 };
