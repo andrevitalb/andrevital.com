62c20935d9d6ba654d67cee6a8f22a8c3fa66c5c
andrevitalb <santiago_vital@outlook.com>
2022-02-06 11:06:16 -0600

    Removed obsolete implementation for mixins

---
+++ src/components/common/mixins/index.jsx
@@ -0,0 +1,32 @@
+export const CustomScrollbar = ({ size }) => `
+	&::-webkit-scrollbar {
+		width: ${size || "5px"};
+		height: ${size || "5px"};
+		border-radius: ${size || "5px"};
+	}
+
+	&::-webkit-scrollbar-track {
+		background: #f1f1f1;
+		border-radius: ${size || "5px"};
+	}
+
+	&::-webkit-scrollbar-thumb {
+		background: #888;
+		border-radius: ${size || "5px"};
+	}
+
+	&::-webkit-scrollbar-thumb:hover {
+		background: #555;
+	}
+`
+
+export const AfterBar = ({ width, height, color }) => `
+    &::after {
+        content: '';
+        display: block;
+        margin-top: 1.5rem;
+        width: ${width};
+        height: ${height};
+        background-color: ${color};
+    }
+`
