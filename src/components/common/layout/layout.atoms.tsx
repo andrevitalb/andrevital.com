d4d6c368dc485d3b9792d6f03d3f03ec5c8260a7
andrevitalb <santiago_vital@outlook.com>
2022-02-07 21:25:32 -0600

    Expanded layout atoms styles

--- src/components/common/layout/layout.atoms.tsx
+++ src/components/common/layout/layout.atoms.tsx
@@ -5,6 +5,6 @@ export const Jumbotron = styled.section`
 	background-image: url('/assets/images/bg.jpg');
 `
 export const JumbotronContent = tw.div`min-h-screen flex items-center text-center`
-export const JumbotronContentLayout = tw.div`flex w-full justify-center`
-export const JumbotronHeader = tw.h1`text-white text-8xl`
+export const JumbotronContentLayout = tw.div`flex flex-col w-full justify-center items-center`
+export const JumbotronHeader = tw.h1`text-white text-8xl font-semibold`
 export const JumbotronText = tw.p`uppercase text-gray-200 text-2xl inline-block px-4 border-gray-300 border-r-2 last:border-r-0`
