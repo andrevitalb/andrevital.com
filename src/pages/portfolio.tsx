734c2d6fcb3db5c3032c5551e57b3e07b6f30b25
andrevitalb <santiago_vital@outlook.com>
2022-02-07 21:28:44 -0600

    Updated portfolio page component structure

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,9 +1,7 @@
 import PortfolioList from "components/Portfolio/PortfolioList"
-import { useLocation } from "react-router-dom"
 
 const Portfolio = () => {
-	const location = useLocation()
-	return <PortfolioList currentPortfolio={location.pathname.split("/")[1]} />
+	return <PortfolioList />
 }
 
 export default Portfolio
