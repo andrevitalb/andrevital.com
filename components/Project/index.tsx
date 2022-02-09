4206817f04cba0492fca85a818b6ce92ac652bd2
andrevitalb <santiago_vital@outlook.com>
2022-02-09 00:25:52 -0600

    Update project component to use atoms

--- components/Project/index.tsx
+++ components/Project/index.tsx
@@ -1,31 +1,34 @@
 import { BaseProject } from "contexts/ProjectsContext"
-import { ProjectJumbotron, ProjectJumbotronLogo } from "./project.atoms"
+import {
+	ProjectContentHolder,
+	ProjectHeaderSection,
+	ProjectHeaderTitle,
+	ProjectJumbotron,
+	ProjectJumbotronLogo,
+	ProjectTagContainer,
+} from "./project.atoms"
 
 export const Project = ({ project }: { project: BaseProject }) => {
 	const { id, name, category, tags, accent } = project
 
 	return (
-		<section className="bg-white">
+		<>
 			<ProjectJumbotron accent={accent}>
 				<ProjectJumbotronLogo
-					src={`/assets/images/portfolio/${category}/${id}/logo.svg`}
+					src={`/images/portfolio/${category}/${id}/logo.svg`}
 					alt={name}
 				/>
 			</ProjectJumbotron>
-			<section className="project__content">
-				<section className="project__section project__section--header">
-					<h1 className="project__title">{name}</h1>
-					<div className="project__tags">
-						<div className="project__tag-container">
-							{tags.map((tag) => (
-								<div key={tag} className="project__tag">
-									<p className="project__tag__text">{tag}</p>
-								</div>
-							))}
-						</div>
-					</div>
-				</section>
-			</section>
-		</section>
+			<ProjectContentHolder>
+				<ProjectHeaderSection>
+					<ProjectHeaderTitle>{name}</ProjectHeaderTitle>
+					<ProjectTagContainer>
+						{tags.map((tag) => (
+							<p key={tag}>{tag}</p>
+						))}
+					</ProjectTagContainer>
+				</ProjectHeaderSection>
+			</ProjectContentHolder>
+		</>
 	)
 }
