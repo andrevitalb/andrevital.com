fa5076cd130a8c10b69b8ea01300f7447cad8f5a
andrevitalb <santiago_vital@outlook.com>
2022-02-09 00:24:42 -0600

    Updated portfolio atoms

--- components/Portfolio/portfolio.atoms.tsx
+++ components/Portfolio/portfolio.atoms.tsx
@@ -1,76 +1,71 @@
-import {
-	absolutePositionCenter,
-	baseTransition,
-} from "components/common/mixins"
-import { Link } from "react-router-dom"
+import { baseTransition } from "components/common/mixins"
+import Link from "next/link"
+import { ReactNode } from "react"
 import tw, { css, styled } from "twin.macro"
 
 export const PortfolioContainer = tw.div`w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3`
 
 // PortfolioItem elements
-export const PortfolioItem = styled.div`
-	${tw`col-span-1 min-h-[300px] relative cursor-pointer overflow-hidden`}
-	&:hover {
-		div:first-child::after {
-			transform: translateX(0);
-		}
-		div:last-child {
-			left: 50%;
-		}
-	}
-`
+export const PortfolioItem = tw.div`col-span-1 min-h-[300px] relative cursor-pointer overflow-hidden`
 export const PortfolioImageContainer = styled.div`
-	${tw`w-full h-full`}
+	${tw`
+		w-full h-full 
+		after:(
+			content-[""]
+			w-full h-full
+			bg-black bg-opacity-60
+			absolute top-0 left-0
+			z-[2] -translate-x-full
+		)
+		group-hover:after:translate-x-0
+	`}
 	${baseTransition()}
     &::after {
-		content: "";
-		${tw`top-0 left-0 w-full h-full absolute`}
 		${baseTransition()}
-		transform: translateX(-100%);
-		background-color: rgba(0, 0, 0, 0.6);
-		z-index: 2;
 	}
 `
 export const PortfolioImage = styled.img`
-	${tw`w-full h-full object-cover hover:scale-105`}
+	${tw`w-full h-full object-cover group-hover:scale-105`}
 	${baseTransition()}
 `
 export const PortfolioItemContent = styled.div`
-	${tw`w-full text-center`}
+	${tw`
+		w-full text-center absolute
+		top-1/2 -left-1/2 
+		-translate-x-1/2 -translate-y-1/2
+		z-[4] 
+		group-hover:left-1/2
+	`}
 	${baseTransition()}
-	${absolutePositionCenter}
-	left: -50%;
-	z-index: 4;
 `
-export const PortfolioItemTitle = tw.h3`text-white text-3xl`
-export const PortfolioItemTagContainer = tw.p`text-lg`
+export const PortfolioItemTitle = tw.h3`text-white text-3xl font-semibold`
+export const PortfolioItemTagContainer = tw.p`text-lg font-light`
 export const PortfolioItemTag = tw.span`py-0.5 px-2 border-white border-r-2 last:border-r-0`
 
 // PortfolioModal elements
 export const PortfolioModalContainer = styled.div<{ open: boolean }>(
 	({ open }) => [
-		tw`fixed top-0 h-full left-0 w-full md:(left-[100px] w-[calc(100%-100px)])`,
+		tw`
+			fixed top-0 left-0 
+			h-full w-full 
+			opacity-0 z-[-1]
+			bg-black bg-opacity-75 
+			md:(ml-[100px] w-[calc(100% - 100px)])
+		`,
 		baseTransition(".5s"),
-		`
-            opacity: 0;
-            background-color: rgba(0, 0, 0, .75);
-            z-index: -1;
-        `,
-		open &&
-			`
-            opacity: 1;
-            z-index: 10;
-            pointer-events: all;
-        `,
+		open && tw`opacity-100 z-10 pointer-events-auto`,
 	],
 )
 
 export const PortfolioModal = styled.div<{ active: boolean }>(({ active }) => [
-	tw`w-[75vw] md:w-[50vw] bg-white text-black`,
+	tw`
+		w-[75vw] md:w-[50vw] 
+		bg-white text-black
+		absolute top-1/2 left-1/2
+		-translate-x-1/2 translate-y-full
+	`,
 	baseTransition(),
-	absolutePositionCenter,
-	`transform: translate(-50%, 100%);`,
-	active && `transform: translate(-50%, -50%);`,
+	active && tw`-translate-y-1/2`,
 ])
 
 const basePortfolioModalButtonStyles = css`
@@ -103,7 +98,7 @@ export const PortfolioModalImage = tw.img`w-full`
 export const PortfolioModalContent = tw.div`p-8`
 export const PortfolioModalHeader = tw.div`block flex-wrap md:flex justify-between content-center mb-4`
 export const PortfolioModalTitle = tw.h3`flex-grow text-3xl`
-export const PorfolioModalTagContainer = tw.h4`flex items-center gap-x-2 md:justify-end flex-grow`
+export const PorfolioModalTagContainer = tw.h4`flex items-center gap-x-3 md:justify-end flex-grow`
 export const PortfolioModalTag = tw.span`inline-block`
 export const PortfolioModalDescription = tw.p`text-xl mb-4`
 export const PortfolioModalCtaContainer = tw.div`flex flex-wrap`
@@ -121,7 +116,22 @@ const basePortfolioModalCtaStyles = css`
 	${baseTransition()}
 `
 
-export const PortfolioModalInternalCta = styled(Link)`
+export const PortfolioModalInternalCta = ({
+	href,
+	children,
+	...props
+}: {
+	href: string
+	children: ReactNode
+}) => {
+	return (
+		<Link href={href} passHref>
+			<InternalCtaBase {...props}>{children}</InternalCtaBase>
+		</Link>
+	)
+}
+
+const InternalCtaBase = styled.a`
 	${basePortfolioModalCtaStyles}
 `
 export const PortfolioModalExternalCta = styled.a`
