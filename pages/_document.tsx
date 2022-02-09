b94762eef52d7a08fa9208633899a64687962cbf
andrevitalb <santiago_vital@outlook.com>
2022-02-09 00:22:30 -0600

    Updated favicon asset routes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -53,68 +53,65 @@ export default class CustomDocument extends Document {
 					<link
 						rel="icon"
 						type="image/ico"
-						href="%PUBLIC_URL%/assets/images/favicons/favicon.ico"
+						href="/images/favicons/favicon.ico"
 					/>
 					<link
 						rel="shortcut icon"
-						href="%PUBLIC_URL%/assets/images/favicons/favicon.png"
+						href="/images/favicons/favicon.png"
 					/>
 					<link
 						rel="apple-touch-icon"
-						href="%PUBLIC_URL%/assets/images/favicons/apple-touch-icon-57x57.png"
+						href="/images/favicons/apple-touch-icon-57x57.png"
 					/>
 					<link
 						rel="apple-touch-icon"
 						sizes="72x72"
-						href="%PUBLIC_URL%/assets/images/favicons/apple-touch-icon-72x72.png"
+						href="/images/favicons/apple-touch-icon-72x72.png"
 					/>
 					<link
 						rel="apple-touch-icon"
 						sizes="114x114"
-						href="%PUBLIC_URL%/assets/images/favicons/apple-touch-icon-114x114.png"
+						href="/images/favicons/apple-touch-icon-114x114.png"
 					/>
 					<link
 						rel="icon"
 						type="image/png"
 						sizes="36x36"
-						href="%PUBLIC_URL%/assets/images/favicons/android-chrome-36x36.png"
+						href="/images/favicons/android-chrome-36x36.png"
 					/>
 					<link
 						rel="icon"
 						type="image/png"
 						sizes="48x48"
-						href="%PUBLIC_URL%/assets/images/favicons/android-chrome-48x48.png"
+						href="/images/favicons/android-chrome-48x48.png"
 					/>
 					<link
 						rel="icon"
 						type="image/png"
 						sizes="72x72"
-						href="%PUBLIC_URL%/assets/images/favicons/android-chrome-72x72.png"
+						href="/images/favicons/android-chrome-72x72.png"
 					/>
 					<link
 						rel="icon"
 						type="image/png"
 						sizes="96x96"
-						href="%PUBLIC_URL%/assets/images/favicons/android-chrome-96x96.png"
+						href="/images/favicons/android-chrome-96x96.png"
 					/>
 					<link
 						rel="icon"
 						type="image/png"
 						sizes="144x144"
-						href="%PUBLIC_URL%/assets/images/favicons/android-chrome-144x144.png"
+						href="/images/favicons/android-chrome-144x144.png"
 					/>
 					<link
 						rel="icon"
 						type="image/png"
 						sizes="192x192"
-						href="%PUBLIC_URL%/assets/images/favicons/android-chrome-192x192.png"
+						href="/images/favicons/android-chrome-192x192.png"
 					/>
 
 					{/* FontAwesome */}
-					<link
-						rel="stylesheet"
-						href="%PUBLIC_URL%/assets/css/all.min.css"
-					/>
+					<link rel="stylesheet" href="/css/all.min.css" />
 
 					{/* Google Fonts */}
 					<link
