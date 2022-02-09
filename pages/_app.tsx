1e6738b32dd62ef84573c059ef74f66f9dfee119
andrevitalb <santiago_vital@outlook.com>
2022-02-08 19:54:53 -0600

    Added ProjectsContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
-import 'styles/globals.css'
+import { ProjectsProvider } from "contexts/ProjectsContext"
+import "styles/globals.css"
 import { GlobalStyles } from "twin.macro"
 
 export default function App<Props>({
-  Component,
-  pageProps,
+	Component,
+	pageProps,
 }: {
-  Component: React.ComponentType<Props>
-  pageProps: Props
+	Component: React.ComponentType<Props>
+	pageProps: Props
 }) {
-  return (
-    <>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
-  )
+	return (
+		<ProjectsProvider>
+			<GlobalStyles />
+			<Component {...pageProps} />
+		</ProjectsProvider>
+	)
 }
