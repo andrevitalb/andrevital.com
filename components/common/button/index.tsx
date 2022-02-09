dd720a5a322612f127d29b645b8a871da8ff3aba
andrevitalb <santiago_vital@outlook.com>
2022-02-09 00:24:57 -0600

    Updated transparent button styles

--- components/common/button/index.tsx
+++ components/common/button/index.tsx
@@ -10,7 +10,7 @@ export interface ButtonProps {
 
 export const buttonColors: Record<Colors, TwStyle> = {
 	gray: tw`bg-gray-400 hover:bg-gray-500 disabled:bg-gray-300`,
-	transparent: tw`bg-transparent border-2 border-gray-200 text-gray-200 hover:(border-gray-300 text-gray-300)`,
+	transparent: tw`bg-transparent border-4 border-white text-white hover:(border-gray-300 text-gray-300)`,
 }
 
 export const buttonSizes: Record<Sizes, TwStyle> = {
