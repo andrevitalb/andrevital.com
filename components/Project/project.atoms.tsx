0187efce0ec1fb4a0d5dcea00cc8e1a204fcb42d
andrevitalb <santiago_vital@outlook.com>
2022-02-09 00:25:18 -0600

    Updated project atoms

--- components/Project/project.atoms.tsx
+++ components/Project/project.atoms.tsx
@@ -5,10 +5,11 @@ export const ProjectJumbotron = styled.div<{ accent: string }>(({ accent }) => [
 	`background: ${accent}`,
 ])
 export const ProjectJumbotronLogo = tw.img`w-[25vw] max-h-[175px]`
+export const ProjectContentHolder = tw.div`bg-white text-black`
 export const ProjectHeaderSection = tw.section`
 	flex items-center justify-between
-	pt-[10vh] px-[8vw] pb-[5vh]
+	py-[7.5vh] px-[8vw]
 	text-base
 `
-export const ProjectHeaderTitle = tw.h1`text-5xl mb-4`
+export const ProjectHeaderTitle = tw.h1`text-5xl font-semibold`
 export const ProjectTagContainer = tw.div`flex items-center gap-3 after:(content-["|"] block ml-3) last:after:hidden`
