ad07bf64a0fbe5bca8cce96d3ce161a69c1d83e0
andrevitalb <santiago_vital@outlook.com>
2022-02-09 22:37:43 -0600

    Added aqua color schemas to button

--- components/common/button/index.tsx
+++ components/common/button/index.tsx
@@ -1,7 +1,7 @@
 import tw, { styled, TwStyle } from "twin.macro"
 
 type Sizes = "md"
-type Colors = "gray" | "transparent"
+type Colors = "gray" | "transparent" | "aqua" | "aquaTransparent"
 
 export interface ButtonProps {
 	size?: Sizes
@@ -11,6 +11,8 @@ export interface ButtonProps {
 export const buttonColors: Record<Colors, TwStyle> = {
 	gray: tw`bg-gray-300 hover:bg-gray-400 disabled:bg-gray-200`,
 	transparent: tw`bg-transparent border-4 border-white text-white hover:(border-gray-200 text-gray-200)`,
+	aqua: tw`bg-aqua-300 border-4 border-aqua-300 text-blue-300 hover:(bg-transparent text-aqua-300)`,
+	aquaTransparent: tw`bg-transparent border-4 border-aqua-300 text-aqua-300 hover:(bg-aqua-300 text-blue-300)`,
 }
 
 export const buttonSizes: Record<Sizes, TwStyle> = {
@@ -22,7 +24,7 @@ export const Button = styled.button(
 		tw`
             py-4 px-12 text-base font-semibold inline-block no-underline
             font-bold text-white
-            disabled:opacity-50 disabled:cursor-default
+            disabled:(opacity-50 cursor-default)
         `,
 		buttonColors[color],
 		buttonSizes[size],
