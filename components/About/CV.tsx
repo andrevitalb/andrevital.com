9d3c6dc9f401ea6e2aa54aac024559067bfca229
andrevitalb <santiago_vital@outlook.com>
2022-02-10 04:24:12 -0600

    Added about page AboutInfo & CV components

---
+++ components/About/CV.tsx
@@ -0,0 +1,115 @@
+import { Tab, TabList, TabPanel, Tabs } from "components/common/tabs"
+import { useCV } from "contexts/CVContext"
+import { isEmpty } from "lodash"
+import tw from "twin.macro"
+import { jobDatesFormatter } from "util/jobDatesFormatter"
+import { MarkdownTextParser } from "util/MarkdownTextParser"
+import { AboutHeader, TechStackBullet, TextHighlightLink } from "./about.atoms"
+
+export const CV = () => {
+	const cv = useCV()
+
+	return (
+		<section tw="flex justify-center items-center h-full w-full py-6">
+			<div tw="max-w-[90vw] md:max-w-[45vw] p-6 md:py-0">
+				<div tw="flex flex-col justify-center">
+					<AboutHeader>Where I've worked</AboutHeader>
+					<div tw="flex mt-2">
+						{!isEmpty(cv) && (
+							<Tabs selectedTab={cv[0].id}>
+								<TabList tw="flex">
+									<div tw="z-[3] w-max">
+										{cv.map(({ id, companyName }) => (
+											<StyledTab key={id} tabName={id}>
+												<span>
+													{companyName.split(" ")[0]}
+												</span>
+											</StyledTab>
+										))}
+									</div>
+									<div tw="px-6 py-3">
+										{cv.map(
+											({
+												id,
+												companyName,
+												companyPageUrl,
+												position,
+												startDate,
+												endDate,
+												descriptionBullets,
+											}) => (
+												<TabPanel key={id} tabName={id}>
+													<CVListHeader>
+														<span>{position}</span>{" "}
+														<span tw="text-aqua-300">
+															@{" "}
+															<TextHighlightLink
+																href={
+																	companyPageUrl
+																}
+																target="_blank"
+																rel="noreferrer"
+															>
+																{companyName}
+															</TextHighlightLink>
+														</span>
+													</CVListHeader>
+													<p tw="text-gray-200 text-lg font-display mb-3">
+														{jobDatesFormatter(
+															startDate,
+															endDate,
+														)}
+													</p>
+													<CVDescriptionBulletList>
+														{descriptionBullets.map(
+															(bullet, index) => (
+																<TechStackBullet
+																	key={index}
+																	tw="my-2"
+																>
+																	<span>
+																		{bullet.map(
+																			(
+																				bulletItem,
+																				index,
+																			) => (
+																				<MarkdownTextParser
+																					key={
+																						index
+																					}
+																					str={
+																						bulletItem
+																					}
+																					tw="text-aqua-300"
+																				/>
+																			),
+																		)}
+																	</span>
+																</TechStackBullet>
+															),
+														)}
+													</CVDescriptionBulletList>
+												</TabPanel>
+											),
+										)}
+									</div>
+								</TabList>
+							</Tabs>
+						)}
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+const StyledTab = tw(Tab)`
+	no-underline relative whitespace-nowrap
+	flex items-center
+	w-full px-5 py-3 bg-transparent
+	border-l-[3px] border-gray-200
+	text-aqua-300 hover:(bg-gray-300 border-aqua-300)
+`
+
+const CVListHeader = tw.h3`text-2xl text-white font-semibold`
+const CVDescriptionBulletList = tw.ul`list-none text-gray-200`
