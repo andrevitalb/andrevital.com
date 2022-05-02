1b69264bd102c72101c75ecf954d97d118551902
andrevitalb <santiago_vital@outlook.com>
2022-05-02 03:47:16 -0500

    Remove obsolete files

--- util/projectGallerySubindexFetcher.ts
+++
@@ -1,4 +0,0 @@
-export const fetchProjectGalleryItemSubindex = (imageUrl: string) => {
-	const dashFragments = imageUrl.split("-")
-	return dashFragments[dashFragments.length - 1].split(".")[0]
-}
--- util/stringReplacer.ts
+++
@@ -1,5 +0,0 @@
-export const replaceAllSubstrings = (
-	initialString: string,
-	search: string,
-	replaceWith: string = "",
-) => initialString.split(search).join(replaceWith)
