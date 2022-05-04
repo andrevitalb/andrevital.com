471c62951a8d649200fab23b232a0f40e04e24a6
andrevitalb <santiago_vital@outlook.com>
2022-05-03 21:23:10 -0500

    Base config for apollo (graphql)

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,24 +1,37 @@
+import { ApolloProvider, NormalizedCacheObject } from "@apollo/client"
 import { CVProvider } from "contexts/CVContext"
 import { ProjectsProvider } from "contexts/ProjectsContext"
+import { useApollo } from "lib/apolloClient"
 import "lightgallery/css/lightgallery.css"
 import { ComponentType } from "react"
 import "styles/globals.css"
 import "styles/lightGallery.css"
 import { GlobalStyles } from "twin.macro"
 
-export default function App<Props>({
+/**
+ * The App component implicitly wraps every page component. This code runs
+ * client-side and server-side.
+ *
+ * See https://nextjs.org/docs/advanced-features/custom-app
+ */
+export default function App<
+	Props extends { initialApolloState?: NormalizedCacheObject },
+>({
 	Component,
 	pageProps,
 }: {
 	Component: ComponentType<Props>
 	pageProps: Props
 }) {
+	const apolloClient = useApollo(pageProps.initialApolloState)
 	return (
-		<ProjectsProvider>
-			<CVProvider>
-				<GlobalStyles />
-				<Component {...pageProps} />
-			</CVProvider>
-		</ProjectsProvider>
+		<ApolloProvider client={apolloClient}>
+			<ProjectsProvider>
+				<CVProvider>
+					<GlobalStyles />
+					<Component {...pageProps} />
+				</CVProvider>
+			</ProjectsProvider>
+		</ApolloProvider>
 	)
 }
