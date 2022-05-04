769c6777b5a0f586f4a8608bc46941984709d921
andrevitalb <santiago_vital@outlook.com>
2022-05-04 16:25:21 -0500

    Update jobsDateFormatter to work with new endDate undefined case

--- packages/frontend/util/jobDatesFormatter.ts
+++ packages/frontend/util/jobDatesFormatter.ts
@@ -1,9 +1,9 @@
 import { JobDate } from "contexts/CVContext"
 
-export const jobDatesFormatter = (startDate: JobDate, endDate: JobDate) => `
+export const jobDatesFormatter = (startDate: JobDate, endDate?: JobDate) => `
     ${startDate.month}${
-	startDate.year !== endDate.year ? ` ${startDate.year} ` : " "
-}- ${endDate.month !== "" ? endDate.month : "Present"}${
-	endDate.year !== "" ? ` ${endDate.year}` : ""
+	startDate.year !== endDate?.year ? ` ${startDate.year} ` : " "
+}- ${!!endDate?.month ? endDate?.month : "Present"}${
+	!!endDate?.year ? ` ${endDate?.year}` : ""
 }
 `
