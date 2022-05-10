13f30fda0fd8a063d90e78c8dbe281c4fc66f15d
André Vital <santiago_vital@outlook.com>
2022-05-09 21:12:08 -0500

    chore: Fix react type issues (#18)
    * Add @ts-ignore to component with current issue due to @types/react
    
    * Add fragment wrapper to Masonry children
    
    * Update outdated import ref

--- packages/frontend/components/common/layout/Masonry.tsx
+++ packages/frontend/components/common/layout/Masonry.tsx
@@ -66,7 +66,7 @@ export const Masonry = ({
 
 	return (
 		<MasonryWrapper className={className} ref={grid}>
-			{items.map((item) => item)}
+			<>{items.map((item) => item)}</>
 		</MasonryWrapper>
 	)
 }
