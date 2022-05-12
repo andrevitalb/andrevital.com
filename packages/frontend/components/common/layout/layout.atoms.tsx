30c259eb6fba94f8df5a062a619e80d960277503
andrevitalb <santiago_vital@outlook.com>
2022-05-12 14:21:15 -0500

    Styles for main home banner

--- packages/frontend/components/common/layout/layout.atoms.tsx
+++ packages/frontend/components/common/layout/layout.atoms.tsx
@@ -11,5 +11,5 @@ export const Jumbotron = styled.section`
 `
 export const JumbotronContent = tw.div`min-h-screen flex items-center text-center`
 export const JumbotronContentLayout = tw.div`flex flex-col w-full justify-center items-center`
-export const JumbotronHeader = tw.h1`text-white text-8xl font-semibold`
-export const JumbotronText = tw.p`uppercase text-gray-100 text-2xl inline-block px-4 border-gray-100 border-r-2 last:border-r-0`
+export const JumbotronHeader = tw.h1`text-white text-6xl md:text-8xl font-semibold`
+export const JumbotronText = tw.p`uppercase text-gray-100 text-lg md:text-2xl inline-block px-4 border-gray-100 border-r-2 last:border-r-0`
