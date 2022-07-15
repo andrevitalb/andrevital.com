55f1ab68abba87b94c863b51a77c94bb23b68ade
andrevitalb <santiago_vital@outlook.com>
2022-07-15 14:10:33 -0500

    Enforced ArticleEntityType typing

--- packages/frontend/lib/hooks/useArticles.ts
+++ packages/frontend/lib/hooks/useArticles.ts
@@ -1,7 +1,11 @@
 import { gql, useQuery } from "@apollo/client"
 import { articleFields } from "lib/fragments/article.fragment"
 import { sortDataByDate } from "util/sortDataByDate"
-import { Article, articleAttributesMapper } from "./useArticle"
+import {
+	Article,
+	articleAttributesMapper,
+	ArticleEntityType,
+} from "./useArticle"
 
 export const getArticlesQuery = gql`
 	query GetArticles {
@@ -20,7 +24,7 @@ export const getArticlesQuery = gql`
 export function useArticles() {
 	const { data: getArticles } = useQuery(getArticlesQuery)
 	const results: Article[] = getArticles?.articles?.data?.flatMap(
-		(project: any) => articleAttributesMapper(project),
+		(article: ArticleEntityType) => articleAttributesMapper(article),
 	)
 
 	return sortDataByDate(results) as Article[]
