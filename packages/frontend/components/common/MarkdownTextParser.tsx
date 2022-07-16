37ed8287fd31466d193733a4474cab02ee452c36
andrevitalb <santiago_vital@outlook.com>
2022-07-16 00:33:17 -0500

    Update previous iteration of custom markdown parser

--- packages/frontend/components/common/MarkdownTextParser.tsx
+++ packages/frontend/components/common/MarkdownTextParser.tsx
@@ -1,58 +1,40 @@
-import { Fragment, ReactNode } from "react"
-import { replaceAllSubstrings } from "util/stringReplacer"
+import { CSSProperties } from "react"
+import ReactMarkdown from "react-markdown"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import materialOceanic from "react-syntax-highlighter/dist/cjs/styles/prism/material-oceanic"
+import remarkGfm from "remark-gfm"
 
-export const MarkdownTextParser = ({
-	str,
-	className,
-}: {
-	str: string
-	className?: string
-}) => {
-	const boldIndexPairs: number[][] = []
-
-	// This will get the start & end indexes of all instances
-	// of strings between "**" (including the "**")
-	Array.from(str.matchAll(/\*{2}/g)).forEach(({ index }, i) => {
-		typeof index === "number" &&
-			(i % 2 === 0
-				? boldIndexPairs.push([index])
-				: boldIndexPairs[boldIndexPairs.length - 1].push(index + 1))
-	})
-	const allBoldIndexes = boldIndexPairs.flat()
-	const formattedStrings: ReactNode[] = []
-
-	const boldStringFormatter = (start: number, end: number) => (
-		<strong className={className}>
-			{replaceAllSubstrings(str.substring(start, end), "**")}
-		</strong>
-	)
-
-	if (!!allBoldIndexes.length) {
-		let lastBoldAdded = 0
-
-		for (let i = 0; i < str.length; i) {
-			if (allBoldIndexes.includes(i)) {
-				const nextIndex = boldIndexPairs[lastBoldAdded][1] + 1
-				formattedStrings.push(boldStringFormatter(i, nextIndex))
-				i = nextIndex
-				lastBoldAdded++
-			} else {
-				const nextIndex = !!boldIndexPairs[lastBoldAdded]
-					? boldIndexPairs[lastBoldAdded][0]
-					: str.length
-				formattedStrings.push(
-					<span>{str.substring(i, nextIndex)}</span>,
-				)
-				i = nextIndex
-			}
-		}
-	} else formattedStrings.push(str)
-
+export const MarkdownTextParser = ({ content }: { content: string }) => {
 	return (
-		<>
-			{formattedStrings.map((subStr, index) => (
-				<Fragment key={index}>{subStr}</Fragment>
-			))}
-		</>
+		<ReactMarkdown
+			remarkPlugins={[remarkGfm]}
+			components={{
+				code({ node, inline, className, children, style, ...props }) {
+					const match = /language-(\w+)/.exec(className || "")
+					return !inline && match ? (
+						<SyntaxHighlighter
+							style={
+								materialOceanic as {
+									[key: string]: CSSProperties
+								}
+							}
+							showLineNumbers
+							wrapLines
+							children={String(children).replace(/\n$/, "")}
+							language={match[1]}
+							PreTag="div"
+							tw="rounded-md"
+							{...props}
+						/>
+					) : (
+						<code className={className} {...props}>
+							{children}
+						</code>
+					)
+				},
+			}}
+		>
+			{content}
+		</ReactMarkdown>
 	)
 }
