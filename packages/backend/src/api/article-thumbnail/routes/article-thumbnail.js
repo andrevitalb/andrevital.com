8b92621372e41b6a1538e8f421c2a70818030732
andrevitalb <santiago_vital@outlook.com>
2022-07-27 21:50:10 -0500

    Create ArticleThumbnail table

---
+++ packages/backend/src/api/article-thumbnail/routes/article-thumbnail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-thumbnail router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::article-thumbnail.article-thumbnail');
