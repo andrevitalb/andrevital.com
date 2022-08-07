0b8d7fdf4725644ab08070aeafa309fc36043e29
andrevitalb <andre@andrevital.com>
2022-08-07 14:34:46 -0500

    Remove old build files

--- packages/backend/dist/config/admin.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ({ env }) => ({
-    auth: {
-        secret: env("ADMIN_JWT_SECRET", "fc6328d2b0514ff901f2ff2640f46d12"),
-    },
-});
