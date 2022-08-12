a5a774f7d2a5637331b8c5ff4f2c15a681fa72c9
André Vital <andre@andrevital.com>
2022-08-12 14:32:55 -0500

    Revert deepNonNullable

--- packages/frontend/util/deepNonNullable.ts
+++ packages/frontend/util/deepNonNullable.ts
@@ -2,6 +2,6 @@ type Primitive = string | number | boolean | bigint | symbol | undefined | null
 
 export type DeepNonNullable<T> = T extends Primitive
 	? NonNullable<T>
-	: T extends Record<string, unknown>
+	: T extends {}
 	? { [K in keyof T]: DeepNonNullable<T[K]> }
 	: NonNullable<T>
