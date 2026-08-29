42befb3bffe50ba368be8e0453b482a180bb0ecb
André Vital <andre@andrevital.com>
2026-08-28 19:16:14 -0600

    chore: remove yarn workspace and legacy packages

--- packages/.eslintrc.base.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	extends: ["plugin:prettier/recommended", "plugin:promise/recommended"],
-	rules: { "@typescript-eslint/no-unused-vars": "off" },
-}
--- packages/backend/src/api/article-thumbnail/controllers/article-thumbnail.ts
+++
@@ -1,9 +0,0 @@
-/**
- *  tag controller
- */
-
-import { factories } from "@strapi/strapi"
-
-export default factories.createCoreController(
-	"api::article-thumbnail.article-thumbnail",
-)
